2cc1bd6626126f11f4ff9bff2f5b60920977b2af
Pranjal0981 <pkshukla988@gmail.com>
2024-04-28 23:02:24 +0530

    Updation

--- src/Unauthorized.jsx
+++ src/Unauthorized.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Unauthorized = () => {
     return (
-        <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-            <h2 className="text-4xl font-bold text-red-600 mb-4">Unauthorized Access</h2>
-            <p className="text-lg text-gray-700 mb-8">You are not authorized to access this page.</p>
+        <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
+            <h2 className="text-4xl font-bold mb-4">Access Denied</h2>
+            <p className="text-lg mb-8">Oops! Looks like you don't have permission to view this page.</p>
             {/* You can add additional elements or styling as needed */}
         </div>
     );
