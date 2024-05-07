63981eb4aca6e1bd30419cc399488ba878d5a568
Pranjal0981 <pkshukla988@gmail.com>
2024-05-07 16:17:55 +0530

    scroll

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Nav from './Nav/Nav';
 import Routing from './Routing/Routing';
 import Footer from './components/Footer';
 import { useSelector } from 'react-redux';
+import ScrollToTop from './ScrollTop';
 
 function App() {
   const {user}=useSelector((state)=>state.user)
   console.log(user?.userType)
   return (
     <>
+    <ScrollToTop/>
       <Nav />
       <Routing />
       <Footer className="overflow-hidden" />
