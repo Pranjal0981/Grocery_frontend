c8f63ae43c32392b525b9ed72e1d878262398404
Pranjal0981 <pkshukla988@gmail.com>
2024-05-23 12:01:17 +0530

    update backend link

--- src/config/axios.jsx
+++ src/config/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 const instance = axios.create(
 
     {
-        baseURL: import.meta.env.VITE_BASE_URL,
+        baseURL: 'https://grocery-backend-w10l.onrender.com',
         withCredentials: true,
 
     });
