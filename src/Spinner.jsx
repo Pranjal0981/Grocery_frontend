15cffb344e74d6ca73dc5425e22299ed8f1bf612
Pranjal0981 <pkshukla988@gmail.com>
2024-06-13 18:19:26 +0530

    Update loader

--- src/Spinner.jsx
+++ src/Spinner.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { RotateLoader } from 'react-spinners';
-
-
+import { HashLoader } from 'react-spinners';
 
 const CustomSpinner = ({ loading }) => {
     return (
-        <div className="absolute left-[50%] top-[50%] z-[-10]" >
-            <RotateLoader color="#16A085" loading={loading} size={20} />
+        <div className="relative left-[40vw] top-[15vh]  z-10">
+            <HashLoader color="#16A085" loading={loading} size={50} />
         </div>
     );
 };
